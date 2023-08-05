b3b6f7ce7ede5279a88208081e9cecc2383c8d8a
dutaahmad <dutaahmadtefur@gmail.com>
2023-08-06 01:11:30 +0700

    to github

--- src/config/database.py
+++ src/config/database.py
@@ -1,21 +1,24 @@
 from dotenv import load_dotenv, dotenv_values, find_dotenv
 
-import firebase_admin;
-from firebase_admin import credentials;
-from firebase_admin import firestore;
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 ENV_DATA = dotenv_values()
 
 FIREBASE_CERT = ENV_DATA.get("FIREBASE_CERT")
 
-print(FIREBASE_CERT)
+print("tipe data FIREBASE_CERT : " + str(type(FIREBASE_CERT)))
 
-cred = credentials.Certificate(
-    cert= FIREBASE_CERT
-);
+FIREBASE_CERT_DICT: dict = json.loads(FIREBASE_CERT)
+
+print("tipe data FIREBASE_CERT_DICT : " + str(type(FIREBASE_CERT_DICT)))
+
+cred = credentials.Certificate(cert=FIREBASE_CERT_DICT)
 
 if cred:
-    print("Credential Validity = ", True);
+    print("Credential Validity = ", True)
 
-firebaseApp = firebase_admin.initialize_app(credential=cred);
-db = firestore.client(firebaseApp);
+firebaseApp = firebase_admin.initialize_app(credential=cred)
+db = firestore.client(firebaseApp)
